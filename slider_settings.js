d06bda5519c27b59750c097bd103be7ea0e04f36
Arondit <57594292+Arondit@users.noreply.github.com>
2019-11-11 02:57:29 +0300

    Add files via upload

---
+++ slider_settings.js
@@ -0,0 +1,23 @@
+﻿
+$(document).ready(function () {
+    $('.slider').slick({
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      prevArrow: '.left_arrow',
+      nextArrow: '.right_arrow',
+      autoplay: true,
+      speed: 1000,
+      easing: 'linear',
+      fade: true,
+      dots: true,
+      responsive: [{
+        breakpoint: 600,
+        settings: {
+            arrows: false,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            dots: false
+        }
+    }]  
+    });
+  })
